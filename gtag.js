5cfc91a0574cacceaa6e99937dfeb792345620cd
Nibs007 <83820201+Nibs007@users.noreply.github.com>
2021-05-11 13:30:08 +0530

    Update gtag.js

--- gtag.js
+++ gtag.js
@@ -1,9 +1,5 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-90VMKHLP8Q"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-  gtag('config', 'G-90VMKHLP8Q');
-</script>
+gtag('config', 'G-90VMKHLP8Q');
